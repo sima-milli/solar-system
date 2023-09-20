5ad8610cebbfa62fed245b03de3e1a69e807a1b2
sima-milli <sima.milli@gmail.com>
2023-09-20 18:52:10 +0200

    add solarSystem class

---
+++ week14-solar-system.py
@@ -0,0 +1,22 @@
+import turtle
+import math
+
+class SolarSystem:
+    def __init__(self, width, height):
+        self.thesun = None
+        self.planets = []
+        self.ssturtle = turtle.Turtle()
+        self.ssturtle.hideturtle()
+        self.ssscreen = turtle.Screen()
+        self.ssscreen.setworldcoordinates(-width/2.0,-height/2.0,width/2.0,height/2.0)
+        self.ssscreen.tracer(50)
+
+    def addPlanet(self, aplanet):
+        self.planets.append(aplanet)
+
+    def addSun(self, asun):
+        self.thesun = asun
+
+    def showPlanets(self):
+        for aplanet in self.planets:
+            print(aplanet)
